8d41f5c51dc8c5d524b8d3a5f2fb43507a9be502
Teemu Ikonen <teemu.ikonen@iki.fi>
2014-09-13 11:49:12 +0800

    unity csv reader

---
+++ csvreader/Main.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Main : MonoBehaviour {
+
+	void Awake() {
+	
+		List<Dictionary<string,object>> data = CSVReader.Read ("example");
+		
+		for(var i=0; i < data.Count; i++) {
+			print ("name " + data[i]["name"] + " " +
+				   "age " + data[i]["age"] + " " +
+			       "speed " + data[i]["speed"] + " " +
+			       "desc " + data[i]["description"]);
+		}
+	
+	}
+
+	// Use this for initialization
+	void Start () {	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
